05e39b45d0b28d4ec824ba66fe404ada8ce8c1d6
Nicolas Faraci <nicolasfaraci@outlook.com>
2022-12-30 10:34:02 +0100

    Remove unused import

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow, Title;
 import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
